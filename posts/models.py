cd0a58e7ab889f49dd3c608bf6c31a7ad5b2bc9b
njambiEvelyne <evelynenganga76@gmail.com>
2025-02-20 10:14:38 +0300

    Instaleed black

--- posts/models.py
+++ posts/models.py
@@ -5,20 +5,20 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-#Django ORM
-# O - Object 
+# Django ORM
+# O - Object
 # R - Relation
 # M - Mapper
-#The Django ORM tool helps with the relation of classes in Python with the tables in the Database
+# The Django ORM tool helps with the relation of classes in Python with the tables in the Database
+
 
 class Posts(models.Model):
-  content = models.CharField(max_length=300, null=True)
+    content = models.CharField(max_length=300, null=True)
 
-  posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
-  def __str__(self):
-    return f"Post by: {self.posted_by.username}"
+    def __str__(self):
+        return f"Post by: {self.posted_by.username}"
 
-  
 
-#Advanced model relatioships
+# Advanced model relatioships
